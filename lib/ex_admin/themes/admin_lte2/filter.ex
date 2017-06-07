5c78b9e30d941e7ba7f85d1d2dd83354b0e418ce
casio <carsten.kraus@gmail.com>
2017-06-07 18:07:19 +0200

    allow custom types in lte2 theme

--- lib/ex_admin/themes/admin_lte2/filter.ex
+++ lib/ex_admin/themes/admin_lte2/filter.ex
@@ -19,7 +19,12 @@ defmodule ExAdmin.Theme.AdminLte2.Filter do
             if scope do
               input type: :hidden, name: :scope, value: scope
             end
-            for field <- fields(defn), do: build_field(field, q, defn)
+            for field <- fields(defn) do
+              field_type_matching = Application.get_env(:ex_admin, :field_type_matching) || %{}
+              {name, type} = field
+              type = Map.get(field_type_matching, type, type)
+              build_field({name, type}, q, defn)
+            end
             for field <- associations(defn), do: build_field(field, q, defn)
           end
           div ".box-footer" do
